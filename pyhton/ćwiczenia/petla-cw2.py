a5ee0d84b27f57b980c069bf21d5a9ecd80253cc
Dominika21 <nietypowa22@o2.pl>
2018-10-02 12:35:41 +0200

    ćwiczenia z pętli for i while

--- pyhton/ćwiczenia/petla-cw2.py
+++ pyhton/ćwiczenia/petla-cw2.py
@@ -6,10 +6,19 @@
 
 
 def main(args):
-    n = int(input("Podaj liczbę n: ")
-    m = int(input("Podaj liczbę m: ")
+    n = int(input("Podaj liczbę n: "))
+    while n < 1: 
+        print("Błędne dane! ")
+        n = int(input("Podaj liczbę n: "))
+        
+    m = int(input("Podaj liczbę m: "))   
+    while m <= n: 
+         print("Błędne dane! ")
+         m = int(input("Podaj liczbę m: "))   
+         
     for liczba in range (n, m +1):
-        print(liczba, ""
+        print(liczba, " ", end="")
+        
     return 0
 
 if __name__ == '__main__':
