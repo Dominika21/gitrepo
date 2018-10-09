c31fd53610bc95159423941828db46935f99d7d8
Dominika21 <nietypowa22@o2.pl>
2018-10-09 12:35:48 +0200

    ćwiczenia na figurach

--- pyhton/figury.py
+++ pyhton/figury.py
@@ -4,27 +4,46 @@
 #  figury.py
 #  
 
-def main(args):
-    a = int(input("Podaj 1 bok prostokąta: "))
-    b = int(input("Podaj 2 bok prostokąta: "))
-    znak = input("Podaj znak: ")
+def prostokat1(a,b,znak):
     for i in range(a):
         for j in range(b):
             print(znak, end='')
         print()
+        
+        
+def prostokat2(a,b,znak):        
+    for i in range(a):
+        for j in range(b):
+            if j == 0  or j == b - 1 or i == 0 or i == a - 1:
+                print(znak, end='')
+            else:
+                print(" ",end='')
+        print()
     
+    
+    
+def choinka1(h, znak):
+    for j in range(h):
+        if j == 0 or j == h - 1:
+            print(znak, end='')
+     
+   
+    
+    
+def choinka2(h, znak):
+  
+def main(args):
     a = int(input("Podaj 1 bok prostokąta: "))
     b = int(input("Podaj 2 bok prostokąta: "))
     znak = input("Podaj znak: ")
-    for i in range(a):
-        for j in range(b):
-            if j == 0  or j == b - 1:
-                print(znak, end='')
-            else
-                print('',end='')
-        print()
-    
+    h = input("Podaj wysokość choinki: ")
+    prostokat1(a,b,znak)
+    print()
+    prostokat2(a,b,znak)
+    print()
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
