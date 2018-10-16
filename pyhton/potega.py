caeb3edb841efb14be6a874c1c27383443151d35
Dominika21 <nietypowa22@o2.pl>
2018-10-16 12:36:06 +0200

    potęga i silnia

--- pyhton/potega.py
+++ pyhton/potega.py
@@ -7,14 +7,17 @@ def potega_it(a, n):
     wynik = 1
     
     for i in range(n):
-        print(wynik)
         wynik = wynik * a 
-    
+        print(wynik)
     return wynik
 
 def main(args):
-    a, n = 3, 4 
-    print("Potęga {} do {} wynosi {}".format(a, n, potega_it(a, n)))
+    # a, n = 3, 4 
+    # print("Potęga {} do {} wynosi {}".format(a, n, potega_it(a, n)))
+    assert(potega_it(0, 2) == 0)
+    assert(potega_it(1, 1) == 1)
+    assert(potega_it(2, 2) == 4)
+    assert(potega_it(4, 2) == 16)
     return 0
 
 if __name__ == '__main__':
