5e4f48ad74c7266d9f68d09727e592662819cb38
Dominika21 <nietypowa22@o2.pl>
2018-10-23 12:36:57 +0200

    liczby23.cpp i tabliczka.cpp i pd

---
+++ cpp/liczby23.cpp
@@ -0,0 +1,28 @@
+/*
+ * liczby23.cpp
+ */
+
+
+#include <iostream>
+
+int liczby2() {
+    int ile = 0;
+    for(int i = 1; i < 10; i++) {
+        for(int j = 1; j < 10; j++) {
+            if(i != j) {
+                cout << i << j << " ";
+                ile++;
+                }
+            }
+        }
+    return ile;
+}
+
+
+int main(int argc, char **argv)
+{
+	cout << "liczba 2-cyfrowyvh: " << liczby2();
+    
+	return 0;
+}
+
---
+++ cpp/tabliczka.cpp
@@ -0,0 +1,35 @@
+/*
+ * bez nazwy.cxx
+ * 
+ 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+void tabliczka(int x, int y) {
+    for (int i = 1; i <= x; i++) {
+         for (int j = 1; j <= y; j++) {
+            cout << i * j << " ";
+            }
+            cout << endl;
+        }
+    }
+
+int main(int argc, char **argv)
+{
+	int m; //deklaracja 
+    m = 0; //inicjacja
+    
+    int n = 0; //definicja
+    
+    cout << "Podaj 1. liczbę: ";
+    cin >> m;
+    cout << "Podaj 2. liczbę: ";
+    cin >> n;
+    tabliczka(m, n);
+	return 0;
+}
+
