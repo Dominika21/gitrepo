4f2302a101ef6858552659aa989a1e84eadc8d52
Dominika21 <nietypowa22@o2.pl>
2018-10-24 08:45:13 +0200

    euklides.c++

--- cpp/liczby23.cpp
+++ cpp/liczby23.cpp
@@ -10,13 +10,21 @@ int liczby2() {
     for(int i = 1; i < 10; i++) {
         for(int j = 1; j < 10; j++) {
             if(i != j) {
-                cout << i << j << " ";
-                ile++;
+            cout << i << j << " ";
+            ile++;
                 }
             }
         }
     return ile;
 }
+
+
+int liczby3() {
+    int ile = 0;
+    for(int i = 1; i < 10; i++) {
+        for(int}
+    
+    }
 
 
 int main(int argc, char **argv)
