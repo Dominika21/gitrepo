67f73c7e9fd67f49e1f294fb19613a8afe185265
Dominika21 <nietypowa22@o2.pl>
2018-12-06 07:56:22 +0100

    prtle

---
+++ cpp/potega.cpp
@@ -0,0 +1,35 @@
+/*
+ * potega.cpp
+ */
+
+
+#include <iostream>
+
+int potega_it(int a, int n) {
+    int wynik = 1;
+    for(int i = 0; i < n  ; i++) {
+        wynik  *= a;
+    
+    }
+    return wynik;
+}
+
+int potega_re(int a, int n) {
+    if (n == 1)  return 1;
+    return potega_re(a, n-1) * a;
+   
+    
+    
+    
+}
+    
+
+int main(int argc, char **argv)
+{
+	int a, n;
+    cout << "Podaj podstawę i wykładnik: ";
+    cin >> a >> n;
+    cout << "Potęga: " << potęga_it(a, n) << endl;
+	return 0;
+}
+
