8d6e906ecff8a09573a237f59a5b6753b40260a5
Dominika21 <nietypowa22@o2.pl>
2018-12-19 08:45:46 +0100

    cw.petle

---
+++ cpp/cw_petle.cpp
@@ -0,0 +1,47 @@
+/*
+ * cw_petle.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void cw01() {
+    int suma = 0;
+    int liczba;
+    while(suma <= 75) {
+    cout << "Podaj liczbę: ";
+    cin >> liczba;
+        suma = suma + liczba;
+    }
+    cout << "Suma liczb; " << suma << endl;
+}
+
+
+void cw02() {
+    int n;
+    int m;
+    for(int n > 0; m
+     cout << " " <<  << " " ;
+     s
+    
+    
+    cout << "Podaj liczbę n: ";
+    while(n < 0) {
+        
+        
+        
+    }
+}
+
+void cw03() {
+    ;
+}
+
+
+int main(int argc, char **argv)
+{
+	cw01();
+	return 0;
+}
+
